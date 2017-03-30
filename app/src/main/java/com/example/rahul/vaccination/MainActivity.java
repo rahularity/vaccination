4f2f6b1082166c165db4258414c1c9115d080d45
Rahul Pandey <rprahulpandey21@gmail.com>
2017-03-31 01:00:19 +0530

    working app as of now, contains main and editor activity

--- app/src/main/java/com/example/rahul/vaccination/MainActivity.java
+++ app/src/main/java/com/example/rahul/vaccination/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.rahul.vaccination;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_editor);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.editor);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context , EditorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
